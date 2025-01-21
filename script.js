ecba38319c32789bcd76418ce78706d3784049eb
lacriman <proskurin.yarik@gmail.com>
2025-01-21 22:51:09 +0100

    feat: implement random compliment generator with no repeats

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const generateButton = document.getElementById('compliments__button');
-let complimentText = document. getElementById('compliments__text')
-generateButton.addEventListener('click', generateCompliment);
+const generateButton = document.getElementById('compliments__button').addEventListener('click', generateCompliment);
+let complimentText = document.getElementById('compliments__text')
 
+const compliments = [
+   'ты красивая',
+   'ты интересная',
+   'ты крутая',
+   'ты красивая',
+   'ты интересная',
+   'ты крутая',
+   'ты красивая',
+   'ты интересная',
+   'ты крутая',
+   'ты красивая',
+   'ты интересная',
+   'ты крутая',
+   'ты умная'
+]
+
+let lastCompliment = null
 function generateCompliment() {
-   complimentText.innerHTML = 'It works';
+   let complimentNumber = Math.floor(Math.random() * compliments.length)
+
+   while (compliments[complimentNumber] === lastCompliment) {
+      complimentNumber = Math.floor(Math.random() * compliments.length)
+   } 
+   lastCompliment = compliments[complimentNumber]
+   complimentText.innerHTML = compliments[complimentNumber];
 }
